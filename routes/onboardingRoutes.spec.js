0146befc7375c2fed81fc4b33880ad64b08d6d85
stellarluminant <stellarluminant@gmail.com>
2019-03-15 08:07:13 -0700

    Add test spec files
    Tests haven't been written yet, but I have the tests I want to write.

---
+++ routes/onboardingRoutes.spec.js
@@ -0,0 +1,11 @@
+// POST /api/onboarding/register
+// Return 201
+
+// POST /api/onboarding/verifyEmail/:key
+// Returns 201
+
+// GET /api/onboarding/confirmInvite/:key
+// Return inviter name and preferred times
+
+// POST /api/onboarding/confirmTime/:key
+// Return 201 
