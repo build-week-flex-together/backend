e17be0fb1cced3f5c16bd652945c3ea568b6da80
stellarluminant <stellarluminant@gmail.com>
2019-03-17 22:33:13 -0700

    Add integrations

--- integrations/texter.js
+++ integrations/texter.js
@@ -1,2 +1,36 @@
 // Should act as a helper module for sending text messages.
-// Probably won't test this due to scope and time limitations.
+
+// Package imports
+const twilio = require('twilio');
+
+// Constructor for Texter
+const Texter = function() {
+    this.accountSid = process.env.TWILIO_ACCOUNT_SID;
+    this.authToken = process.env.TWILIO_AUTH_TOKEN;
+    this.phoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
+    if (!accountSid)
+        console.error('Your Twilio Account SID appears to be missing. Check your dotenv setup.');
+
+    if (!authToken)
+        console.error('Your Twilio Auth Token appears to be missing. Check your dotenv setup.');
+
+    if (!phoneNumber)
+        console.error('Your Twilio Phone Number appears to be missing. Check your dotenv setup.');
+    
+    this.client = twilio(accountSid, authToken);
+};
+
+// sendMessage returns a promise
+Texter.prototype.sendMessage = function(recipient, text) {
+    const msg = {
+        body: text,
+        from: this.phoneNumber,
+        to: recipient
+    };
+
+    return this.client.messages.create(msg);
+};
+
+// Export for use in routes
+module.exports = Texter;
